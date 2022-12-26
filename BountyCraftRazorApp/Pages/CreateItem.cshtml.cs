3173d4bae6f6b1ddd85b2d96149e9455db059263
msyudesu <msyudesu@gmail.com>
2022-12-26 01:58:39 -0500

    First pass of home page layout. Added razor pages for inventory, itemdb and create item.

---
+++ BountyCraftRazorApp/Pages/CreateItem.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BountyCraftRazorApp.Pages
+{
+    public class CreateItemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
