38170080146caa8509bd04b7a114f4ca133b3b1a
msyudesu <msyudesu@gmail.com>
2022-12-26 02:26:51 -0500

    Continuing to setup project structure. Added Models and installed EFCore NuGet packages

---
+++ BountyCraftRazorApp/Models/ItemModel.cs
@@ -0,0 +1,26 @@
+﻿using BountyCraftLibrary;
+
+namespace BountyCraftRazorApp.Models
+{
+    public class ItemModel
+    {
+        public string Name { get; set; }
+        public string Realm { get; set; }
+        public string Mold_type { get; set; }
+        public int Mold_quantity { get; set; }
+        public int Stardust { get; set; }
+        public string Recipe { get; set; }
+        public Dictionary<string, int> Alchemy { get; set; }
+
+        public ItemModel(string name, string realm, string mold_type, int mold_quantity, int stardust, string recipe, Dictionary<string, int> alchemy)
+        {
+            Name = name;
+            Realm = realm;
+            Mold_quantity = mold_quantity;
+            Mold_type = mold_type;
+            Stardust = stardust;
+            Recipe = recipe;
+            Alchemy = alchemy;
+        }
+    }
+}
